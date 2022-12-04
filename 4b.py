cd9f59e0b379d91a985650359862e2f7ef0a498f
Wiwiweb <wiwiweb2@gmail.com>
2022-12-03 23:07:06 -0800

    Day 4

---
+++ 4b.py
@@ -0,0 +1,22 @@
+# from itertools import *
+# from more_itertools import *
+# from string import *
+
+sum = 0
+with open("4-input.txt") as file:
+    lines = file.read().split('\n')
+    for line in lines:
+      pair = line.split(',')
+      first = pair[0].split('-')
+      second = pair[1].split('-')
+      first = [int(first[0]), int(first[1])]
+      second = [int(second[0]), int(second[1])]
+      if first[0] <= second[0] and second[0] <= first[1] or first[0] <= second[1] and second[1] <= first[1]:
+        sum += 1
+        print(first, second)
+      elif second[0] <= first[0] and first[0] <= second[1] or second[0] <= first[1] and first[1] <= second[1]:
+        sum += 1
+        print(first, second)
+
+
+print(sum)
