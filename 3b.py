fa090b1420df08b4931e58347cbb862dfa270b09
Wiwiweb <wiwiweb2@gmail.com>
2022-12-03 23:06:48 -0800

    3b improvments after-the-fact

--- 3b.py
+++ 3b.py
@@ -1,18 +1,16 @@
+import more_itertools
+import string
+
 sum = 0
 with open("3-input.txt") as file:
   lines = file.read().split('\n')
-  i = 0
-  while i < len(lines):
-    one = set(lines[i])
-    two = set(lines[i+1])
-    three = set(lines[i+2])
-    common = one.intersection(two, three)
-    (common,) = common
+  for group in more_itertools.chunked(lines, 3):
+    common = set(group[0]) & set(group[1]) & set(group[2])
+    common = more_itertools.one(common)
     if common.isupper():
       common = common.lower()
       sum += 26
     common_digit = ord(common) - 96
     sum += common_digit
-    i += 3
 
 print(sum)
